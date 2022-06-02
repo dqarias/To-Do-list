7c606a275b4ddbfdc6adf3c8154cf21cf7802b64
dqarias <dino.quispea@gmail.com>
2022-06-02 14:38:02 -0500

    Add array in local storage

--- src/index.js
+++ src/index.js
@@ -4,78 +4,75 @@ import './style.css';
 
 const todos = document.querySelector('.todos__list');
 const btnAdd = document.querySelector('#btn-add');
-const todoAdd = document.querySelector('#todo-add')
+const todoAdd = document.querySelector('#todo-add');
 
-class todoTask {
-  constructor(index, description, completed = false ){
+class Todo {
+  constructor(index, description, completed){
     this.index = index;
     this.description = description;
     this.completed = completed;
   }
 }
 
-const todoData = [];
+
+const todoHtml = (todo) => {
+  todos.innerHTML += `<li class="todo_list">
+  <div>
+     <input type="checkbox" id="todo${todo.index}" name="todo${todo.index}">
+     <label for="todo${todo.index}">${todo.description}</label>
+  </div>
+  <button onclick="removeTodo(${todo.index})" class="btn">
+     <i class="fa-solid fa-ellipsis-vertical"></i>
+     <i  class="fa-solid fa-trash"></i>
+  </button>
+ </li>` 
+}
+
+const getTodo =() => {
+  const todos = localStorage.getItem('todoData')
+  if(todos){
+   return JSON.parse(todos);
+  } else {
+   return [];
+  }
+ }
+const todoData = getTodo();
+
+const storeTodo = (store) => {
+  localStorage.setItem('todoData', JSON.stringify(store));
+}
 
 const addTodo = () => {
-  const todo = new todoTask(1, todoAdd.value)
+  const todo = new Todo(todoData.length + 1, todoAdd.value, false)
   if(todoAdd.value){
-    todos.innerHTML += ` <li class="todo_list">
-    <div>
-       <input type="checkbox" id="todo${todo.index}" name="todo${todo.index}">
-       <label for="todo${todo.index}">${todo.description}</label>
-    </div>
-    <button class="btn">
-       <i class="fa-solid fa-ellipsis-vertical"></i>
-    </button>
-   </li>` 
+  todoHtml(todo)
+  todoData.push(todo);
+  console.log(todoData)
+  storeTodo(todoData);
+  return todoData;
   } else {
     console.log("The input is empty please try again ")
   }
-  
 }
-/****Add*****/
 
-const renderData = () => {
+const renderTodo = () => {
+ 
   console.log("Visto de afuera",todoData)
-  todoData.forEach((todo) => {
-     todos.innerHTML += ` <li class="todo_list">
-     <div>
-        <input type="checkbox" id="todo${todo.index}" name="todo${todo.index}">
-        <label for="todo${todo.index}">${todo.description}</label>
-     </div>
-     <button class="btn">
-        <i class="fa-solid fa-ellipsis-vertical"></i>
-     </button>
-    </li>`   
-    /* const todoList = document.createElement('li');
-    const checkbox = document.createElement('div');
-    const inputTodo = document.createElement('input');
-    const labelTodo = document.createElement('label');
-    const btnTodo = document.createElement('button');
-    todoList.classList.add('todo_list');
-    inputTodo.setAttribute('type', 'checkbox');
-    inputTodo.setAttribute('id', `todo${todo.index}`);
-    inputTodo.setAttribute('name', `todo${todo.index}`);
-    if (todo.completed) {
-      inputTodo.setAttribute('checked', true);
-    }
-    labelTodo.setAttribute('for', `todo${todo.index}`);
-    labelTodo.innerHTML = `${todo.description}`;
-    btnTodo.classList.add('btn');
-    btnTodo.innerHTML = '<i class="fa-solid fa-ellipsis-vertical"></i>';
-    checkbox.appendChild(inputTodo);
-    checkbox.appendChild(labelTodo);
-    todoList.appendChild(checkbox);
-    todoList.appendChild(btnTodo);
-    todos.appendChild(todoList); */
+  todoData.forEach((todo) => { 
+  todoHtml(todo)
   });
+  
 };
 
+renderTodo();
 
 btnAdd.addEventListener('click', ()=>{
-  
-  console.log("holi",todoData)
+
   addTodo()
+  todoAdd.value='';
   console.log(todoAdd.value)
   });
+
+
+
 
