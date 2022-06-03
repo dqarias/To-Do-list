f9991f9c13943879f01148a2c15d7e4cfab37be1
dqarias <dino.quispea@gmail.com>
2022-06-03 17:34:36 -0500

    Fix linter errors

--- src/index.js
+++ src/index.js
@@ -2,14 +2,17 @@ import '@fortawesome/fontawesome-free/css/all.css';
 import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
-import {addTodo, editTodo, deleteTodo, getTodo} from './modules/action.js'
+// eslint-disable-next-line import/no-cycle
+import {
+  addTodo, editTodo, deleteTodo, getTodo,
+} from './modules/action.js';
 
 const todos = document.querySelector('.todos__list');
 const btnAdd = document.querySelector('#btn-add');
 const todoAdd = document.querySelector('#todo-add');
 
-console.log(todos)
-
+console.log(todos);
+const todoData = getTodo();
 const todoHtml = (todo) => {
   todos.innerHTML += `<li id="${todo.index}" class="todo_list">
   <div>
@@ -19,83 +22,75 @@ const todoHtml = (todo) => {
   <button id="${todo.index}" class="btn btn-remove">
      <i class="fa-solid fa-trash"></i>
   </button>
- </li>` 
+ </li>`;
 
- //Add Event Listener for edit each single book
+  // Add Event Listener for edit each single book
 
- console.log("todoHtml", todoData)
- const todoList = document.querySelectorAll(".todo_list")
- console.log(todoList)
- todoList.forEach((singleTodo)=>{
- singleTodo.addEventListener('dblclick', ()=>{
- console.log("Start to edit")
- const editSingleTodo = getTodo();
-  console.log("Print getTodo from event Listener", editSingleTodo);
- const editIndex = singleTodo.getAttribute('id')
- console.log("Start to editing this one", editIndex, editSingleTodo[editIndex-1].description )
- singleTodo.innerHTML = `
+  console.log('todoHtml', todoData);
+  const todoList = document.querySelectorAll('.todo_list');
+  console.log(todoList);
+  todoList.forEach((singleTodo) => {
+    singleTodo.addEventListener('dblclick', () => {
+      console.log('Start to edit');
+      const editSingleTodo = getTodo();
+      console.log('Print getTodo from event Listener', editSingleTodo);
+      const editIndex = singleTodo.getAttribute('id');
+      console.log('Start to editing this one', editIndex, editSingleTodo[editIndex - 1].description);
+      singleTodo.innerHTML = `
  <li class= "edit_list" id= "edit${editIndex}"> 
        <div>
        <i class="icon fa-solid fa-check"></i>
-         <input class="edit_list-input" id="input-edit${editIndex}" type="text"  value="${editSingleTodo[editIndex-1].description}">
+         <input class="edit_list-input" id="input-edit${editIndex}" type="text"  value="${editSingleTodo[editIndex - 1].description}">
        </div>
        <button id="btn-edit${editIndex}" type ="submit" class="btn" > <i class="fa-solid fa-pen"></i></button>
 </li>
  `;
 
- const todoEdit = document.querySelector(`#btn-edit${editIndex}`)
- console.log("todoEdit", todoEdit)
- todoEdit.addEventListener('click', (e)=>{
-  e.preventDefault();
-  const inputEdit = document.querySelector(`#input-edit${editIndex}`)
- console.log("btnEdit", inputEdit)
- editTodo(editIndex, inputEdit.value)
- renderTodo();
- })
+      const todoEdit = document.querySelector(`#btn-edit${editIndex}`);
+      console.log('todoEdit', todoEdit);
+      todoEdit.addEventListener('click', (e) => {
+        e.preventDefault();
+        const inputEdit = document.querySelector(`#input-edit${editIndex}`);
+        console.log('btnEdit', inputEdit);
+        editTodo(editIndex, inputEdit.value);
+        // eslint-disable-next-line no-use-before-define
+        renderTodo();
+      });
+    });
+  });
 
-   })
- }) 
+  // Add Event Listener for delete each single book
 
- //Add Event Listener for delete each single book
-
- console.log("todoHtml", todoData)
- const removeBtn = document.querySelectorAll(".btn-remove")
- console.log(removeBtn)
- removeBtn.forEach((singleTodo)=>{
- singleTodo.addEventListener('click', ()=>{
- deleteTodo(singleTodo.getAttribute('id'))
-     
-   })
- }) 
-}
-
-
-const todoData = getTodo();
+  console.log('todoHtml', todoData);
+  const removeBtn = document.querySelectorAll('.btn-remove');
+  console.log(removeBtn);
+  removeBtn.forEach((singleTodo) => {
+    singleTodo.addEventListener('click', () => {
+      deleteTodo(singleTodo.getAttribute('id'));
+    });
+  });
+};
 
 const renderTodo = () => {
   const todoData = getTodo();
-  console.log("Data in render",todoData)
+  console.log('Data in render', todoData);
   todos.innerHTML = '';
-    todoData.forEach((todo) => { 
-      todoHtml(todo)
-      });   
+  todoData.forEach((todo) => {
+    todoHtml(todo);
+  });
 };
 
 renderTodo();
 
-todoAdd .addEventListener('keyup', (e)=>{
+todoAdd.addEventListener('keyup', (e) => {
   if (e.key === 'Enter') {
-    addTodo()
+    addTodo();
   }
-  });
+});
 
-btnAdd.addEventListener('click', ()=>{
-  addTodo()
-  console.log(todoAdd.value)
-  });
+btnAdd.addEventListener('click', () => {
+  addTodo();
+  console.log(todoAdd.value);
+});
 
-
-export {renderTodo, todoHtml}
-
-
-
+export { renderTodo, todoHtml };
--- src/modules/action.js
+++ src/modules/action.js
@@ -1,56 +1,54 @@
-import {renderTodo, todoHtml} from '../index.js'
-import Todo from './todo.js'
+// eslint-disable-next-line import/no-cycle
+import { renderTodo, todoHtml } from '../index.js';
+import Todo from './todo.js';
 
 const todoAdd = document.querySelector('#todo-add');
 
 const getTodo = () => {
-    const todoData = localStorage.getItem('todoData')
-    if(todoData){
-     return JSON.parse(todoData);
-    } else {
-     return [];
-    }
-   }
-  
-  const storeTodo = (store) => {
-    localStorage.setItem('todoData', JSON.stringify(store));
+  const todoData = localStorage.getItem('todoData');
+  if (todoData) {
+    return JSON.parse(todoData);
   }
+  return [];
+};
 
-  const addTodo = () => {
+const storeTodo = (store) => {
+  localStorage.setItem('todoData', JSON.stringify(store));
+};
 
-    if(todoAdd.value){
+const addTodo = () => {
+  if (todoAdd.value) {
     const todoData = getTodo();
-    const todo = new Todo(todoData.length + 1, todoAdd.value, false)
-    todoHtml(todo)
+    const todo = new Todo(todoData.length + 1, todoAdd.value, false);
+    todoHtml(todo);
     todoData.push(todo);
-    console.log("todo Data ADD",todoData)
+    console.log('todo Data ADD', todoData);
     storeTodo(todoData);
-    todoAdd.value='';
-    } else {
-      console.log("The input is empty please try again ")
-    }
+    todoAdd.value = '';
+  } else {
+    console.log('The input is empty please try again ');
   }
+};
 
-  const editTodo = (index, value) => {
-    const todoDataEdit = getTodo();
-    todoDataEdit[index-1].description = value;
-    storeTodo(todoDataEdit)
-  } 
+const editTodo = (index, value) => {
+  const todoDataEdit = getTodo();
+  todoDataEdit[index - 1].description = value;
+  storeTodo(todoDataEdit);
+};
 
-  const deleteTodo = (index) =>{
-    const li = document.getElementById(`todos${index}`);
-    let Todos = getTodo();
-    const todoDataRemoved = Todos.filter(task => task.index !== Number(index));
-    todoDataRemoved.forEach((task,index) => {
-      task.index = index + 1;
-    }); 
-    console.log("Sorting Ids",todoDataRemoved)
-    storeTodo(todoDataRemoved)
-    renderTodo()
-    //li.remove();
-  }
+const deleteTodo = (index) => {
+  // const li = document.getElementById(`todos${index}`);
+  const Todos = getTodo();
+  const todoDataRemoved = Todos.filter((task) => task.index !== Number(index));
+  todoDataRemoved.forEach((task, index) => {
+    task.index = index + 1;
+  });
+  console.log('Sorting Ids', todoDataRemoved);
+  storeTodo(todoDataRemoved);
+  renderTodo();
+  // li.remove();
+};
 
-
-export  {addTodo, deleteTodo, editTodo, getTodo, storeTodo}
-
-  
+export {
+  addTodo, deleteTodo, editTodo, getTodo, storeTodo,
+};
