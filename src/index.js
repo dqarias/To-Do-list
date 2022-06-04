75ba4c9061e7620e678d5085701cd9f791fd34b3
dqarias <dino.quispea@gmail.com>
2022-06-04 01:24:42 -0500

    Add function checkbox completed

--- src/index.js
+++ src/index.js
@@ -3,25 +3,39 @@ import '@fortawesome/fontawesome-free/js/all.js';
 import './style.css';
 
 import {
-  addTodo, editTodo, deleteTodo, getTodo,
+  addTodo, editTodo, deleteTodo, getTodo, storeTodo
 } from './modules/action.js';
+import statusTodo from './modules/status.js';
 
 const todos = document.querySelector('.todos__list');
 const btnAdd = document.querySelector('#btn-add');
 const todoAdd = document.querySelector('#todo-add');
 
 const todoHtml = (todo) => {
-  todos.innerHTML += `<li id="${todo.index}" class="todo_list">
-  <div>
-     <input type="checkbox" id="todo${todo.index}" name="todo${todo.index}">
-     <label for="todo${todo.index}">${todo.description}</label>
-  </div>
-  <button id="${todo.index}" class="btn btn-remove">
-     <i class="fa-solid fa-trash"></i>
-  </button>
- </li>`;
+  if(todo.completed){
+    todos.innerHTML += `<li id="${todo.index}" class="todo_list">
+    <div>
+       <input class="checkbox-input" type="checkbox" id="todo${todo.index}" name="todo${todo.index}" checked>
+       <label for="todo${todo.index}">${todo.description}</label>
+    </div>
+    <button id="${todo.index}" class="btn btn-remove">
+       <i class="fa-solid fa-trash"></i>
+    </button>
+   </li>`;
+  }else{
+    todos.innerHTML += `<li id="${todo.index}" class="todo_list">
+    <div>
+       <input class="checkbox-input" type="checkbox" id="todo${todo.index}" name="todo${todo.index}">
+       <label for="todo${todo.index}">${todo.description}</label>
+    </div>
+    <button id="${todo.index}" class="btn btn-remove">
+       <i class="fa-solid fa-trash"></i>
+    </button>
+   </li>`;
+  }
+ 
 
-  // Add Event Listener for edit each single book
+  // Add Event Listener for edit each single todo
 
   const todoList = document.querySelectorAll('.todo_list');
   todoList.forEach((singleTodo) => {
@@ -48,13 +62,29 @@ const todoHtml = (todo) => {
     });
   });
 
-  // Add Event Listener for delete each single book
+  // Add Event Listener for delete each single todo
   const removeBtn = document.querySelectorAll('.btn-remove');
   removeBtn.forEach((singleTodo) => {
     singleTodo.addEventListener('click', () => {
       deleteTodo(singleTodo.getAttribute('id'));
     });
   });
+
+  //Add Event Listener for Checkbox
+
+  const checkboxStatus = document.querySelectorAll('.checkbox-input')
+  console.log("checboxstatus", checkboxStatus)
+  checkboxStatus.forEach((checkboxTodo) => {
+    checkboxTodo.addEventListener('change',()=>{
+      const todoIndex = checkboxTodo.getAttribute('id');
+      const todoId = todoIndex.slice(4)
+      console.log("Click on change", todoId)
+      statusTodo(checkboxTodo, todoId);
+    })
+  });
+
+ //Add Event Listener for delete all todos completed
+
 };
 
 const renderTodo = () => {
